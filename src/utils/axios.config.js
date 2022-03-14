aa4bbba66fb652ff4e77a93ad77f0a40329c04d2
Tung Le <tung.le@saigontechnology.com>
2022-03-14 21:48:41 +0700

    integrate otp api send otp code and verify otp code

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import AuthHelpers from "./AuthHelpers";
 
 // export const signalRUrl = process.env.REACT_APP_API_URL || "http://localhost:4000";
-export const signalRUrl = "http://localhost:4000";
+export const signalRUrl =
+  "https://70900c7a-77e1-4fe4-a7fa-f8bf440d0adb.mock.pstmn.io";
 export const baseUrl = signalRUrl;
 
 // Set config defaults when creating the instance
